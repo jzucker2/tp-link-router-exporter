3d4bc9d0ea98dab4fe1ffc62af7c997499e54dac
Jordan Zucker <jordan.zucker@gmail.com>
2024-03-07 12:12:59 -0800

    And clean up a bit

--- tp_link_router_exporter/app/clients/collector.py
+++ tp_link_router_exporter/app/clients/collector.py
@@ -1,3 +1,4 @@
+import dataclasses
 from flask import current_app as app
 from ..utils import global_get_now
 from ..metrics import Metrics
@@ -36,8 +37,68 @@ class Collector(object):
             scrape_event=event.value,
         ).inc()
 
+    @property
+    def router_ip(self):
+        return self.router_client.router_ip
+
+    def _authorize(self):
+        self.router_client.authorize()
+
+    def _logout(self):
+        self.router_client.logout()
+
+    def _get_firmware(self):
+        return self.router_client.get_firmware()
+
+    def _get_status(self):
+        return self.router_client.get_status()
+
     def test_debug_router(self):
-        return self.router_client.test_debug()
+        log.info('test_debug')
+        try:
+            # authorizing
+            a_m = (f'attempting to authorize at '
+                   f'self.router_ip: {self.router_ip}')
+            log.info(a_m)
+            self._authorize()
+            sa_m = (f'self.router_ip: {self.router_ip} '
+                    f'succeeded at auth')
+            log.info(sa_m)
+            # Get firmware info - returns Firmware
+            firmware = self._get_firmware()
+            log.info(f'router firmware: {firmware}')
+            # firmware_dict = dataclasses.asdict(firmware)
+            # log.info(f'firmware_dict: {firmware_dict}')
+
+            # Get status info - returns Status
+            status = self._get_status()
+            log.info(f'router status: {status}')
+            if status:
+                clients_total = status.wifi_clients_total
+                log.info(f'clients_total: {clients_total}')
+                Metrics.ROUTER_WIFI_CLIENTS_TOTAL.set(
+                    clients_total)
+                Metrics.ROUTER_WIRED_CLIENTS_TOTAL.set(
+                    status.wired_total)
+                Metrics.ROUTER_CLIENTS_TOTAL.set(
+                    status.clients_total)
+                Metrics.ROUTER_MEMORY_USAGE.set(
+                    status.mem_usage)
+                Metrics.ROUTER_CPU_USAGE.set(
+                    status.cpu_usage)
+            status_dict = dataclasses.asdict(status)
+            log.info(f'status_dict: {status_dict}')
+        except Exception as unexp:
+            u_m = (f'self.router_ip: {self.router_ip} '
+                   f'got exception unexp: {unexp}')
+            log.error(u_m)
+
+        finally:
+            # always logout as TP-Link Web
+            # Interface only supports upto 1 user logged
+            l_m = f'now logging out from self.router_ip: {self.router_ip}'
+            log.info(l_m)
+            self._logout()
 
     def get_router_metrics(self):
         # self._inc_voltage_event(event)
