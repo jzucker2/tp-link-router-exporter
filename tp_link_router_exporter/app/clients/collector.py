cba8d99c302c6918f8064bc77862ee55e8efbd06
Jordan Zucker <jordan.zucker@gmail.com>
2024-03-22 16:57:10 -0700

    And that's a bit more

--- tp_link_router_exporter/app/clients/collector.py
+++ tp_link_router_exporter/app/clients/collector.py
@@ -23,6 +23,10 @@ class InvalidRouterFirmwarePropertyCollectorException(CollectorException):
     pass
 
 
+class InvalidClientConnectionTypeCollectorException(CollectorException):
+    pass
+
+
 class Collector(object):
     DEFAULT_ROUTER_NAME = 'default'
 
@@ -114,27 +118,44 @@ class Collector(object):
         self._inc_scrape_event(event)
         return devices
 
+    def _get_client_connection_type_value(self, status, connection_type):
+        if connection_type == ClientConnectionTypes.WIRED:
+            return status.wifi_clients_total
+        elif connection_type == ClientConnectionTypes.WIFI:
+            return status.wired_total
+        elif connection_type == ClientConnectionTypes.TOTAL:
+            return status.clients_total
+        elif connection_type == ClientConnectionTypes.GUEST:
+            return status.guest_clients_total
+        e_m = f'invalid connection_type: {connection_type}'
+        log.error(e_m)
+        raise InvalidClientConnectionTypeCollectorException(e_m)
+
+    def _record_connected_client_count_metrics(self, status):
+        if not status:
+            return
+        log.debug(f'got status: {status}')
+        for connection_type in ClientConnectionTypes.metrics_types_list():
+            client_count = self._get_client_connection_type_value(
+                status,
+                connection_type)
+            Metrics.ROUTER_CONNECTED_CLIENTS_TOTAL.labels(
+                router_name=self.router_name,
+                connection_type=connection_type.label_string,
+            ).set(client_count or 0)
+
     def _record_status_metrics(self, status):
         if not status:
             return
-        Metrics.ROUTER_CONNECTED_CLIENTS_TOTAL.labels(
-            router_name=self.router_name,
-            connection_type=ClientConnectionTypes.WIFI.label_string,
-        ).set(status.wifi_clients_total)
-        Metrics.ROUTER_CONNECTED_CLIENTS_TOTAL.labels(
-            router_name=self.router_name,
-            connection_type=ClientConnectionTypes.WIRED.label_string,
-        ).set(status.wired_total)
-        Metrics.ROUTER_CONNECTED_CLIENTS_TOTAL.labels(
-            router_name=self.router_name,
-            connection_type=ClientConnectionTypes.TOTAL.label_string,
-        ).set(status.clients_total)
+        self._record_connected_client_count_metrics(status)
+        # now do memory and cpu
         Metrics.ROUTER_MEMORY_USAGE.labels(
             router_name=self.router_name,
         ).set(status.mem_usage or 0)
         Metrics.ROUTER_CPU_USAGE.labels(
             router_name=self.router_name,
         ).set(status.cpu_usage or 0)
+        # now WAN IPv4 uptime
         Metrics.ROUTER_WAN_IPV4_UPTIME.labels(
             router_name=self.router_name,
         ).set(status.wan_ipv4_uptime or 0)
@@ -212,13 +233,13 @@ class Collector(object):
         if not reservations:
             return
         # FIXME: actually implement this!
-        log.debug(f'got ipv4 reservations: {reservations}')
+        log.info(f'got ipv4 reservations: {reservations}')
 
     def _record_ipv4_dhcp_leases(self, leases):
         if not leases:
             return
         # FIXME: actually implement this!
-        log.debug(f'got ipv4 dhcp leases: {leases}')
+        log.info(f'got ipv4 dhcp leases: {leases}')
 
     # actual part where we decide what metrics to scrape
     def _get_and_record_router_metrics(self):
@@ -240,20 +261,21 @@ class Collector(object):
         devices = self._get_devices(status)
         self._record_devices_metrics(devices)
 
-        # # Get IPv4 status
+        # Get IPv4 status
+        # FIXME: get_ipv4_status raises an exception in underlying client
         # ipv4_status = self._get_ipv4_status()
-        # log.debug(f'router ipv4_status: {ipv4_status}')
+        # log.info(f'router ipv4_status: {ipv4_status}')
         # self._record_ipv4_status_metrics(ipv4_status)
-        #
-        # # Get IPv4 reservations
-        # ipv4_reservations = self._get_ipv4_reservations()
-        # log.debug(f'router ipv4_reservations: {ipv4_reservations}')
-        # self._record_ipv4_reservations(ipv4_reservations)
-        #
-        # # Get IPv4 dhcp leases
-        # ipv4_leases = self._get_ipv4_dhcp_leases()
-        # log.debug(f'router ipv4_leases: {ipv4_leases}')
-        # self._record_ipv4_dhcp_leases(ipv4_leases)
+
+        # Get IPv4 reservations
+        ipv4_reservations = self._get_ipv4_reservations()
+        log.debug(f'router ipv4_reservations: {ipv4_reservations}')
+        self._record_ipv4_reservations(ipv4_reservations)
+
+        # Get IPv4 dhcp leases
+        ipv4_leases = self._get_ipv4_dhcp_leases()
+        log.debug(f'router ipv4_leases: {ipv4_leases}')
+        self._record_ipv4_dhcp_leases(ipv4_leases)
 
     # handles flow, including log in/out
     def _execute_get_router_metrics(self):
