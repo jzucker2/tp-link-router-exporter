536560f6e9ff04ea3a69e2a761511b86d4c4b615
Jordan Zucker <jordan.zucker@gmail.com>
2024-03-24 19:43:56 -0700

    Getting useful now

---
+++ tp_link_router_exporter/app/clients/device_cache.py
@@ -0,0 +1,37 @@
+from flask import current_app as app
+
+
+log = app.logger
+
+
+# TODO: need to include date checked so I can get all the other devices
+class DeviceCacheException(Exception):
+    pass
+
+
+class DeviceCache(object):
+    def __init__(self):
+        super().__init__()
+        self._devices = {}
+
+    @property
+    def devices(self):
+        return self.devices
+
+    def get_key(self, device):
+        key = str(device.macaddress)
+        return key
+
+    def add_device(self, device):
+        key = self.get_key(device)
+        self.devices[key] = device
+
+    def has_device(self, device):
+        key = self.get_key(device)
+        return bool(key in self.devices)
+
+    def check_device(self, device):
+        if self.has_device(device):
+            self.add_device(device)
+            return True
+        self.add_device(device)
